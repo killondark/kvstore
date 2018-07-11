1e820bb9a98401bb515b5878f03de216cd641aed
Alexander Marychev <killondark@gmail.com>
2018-07-11 15:12:43 +0300

    Added crud to KVstore.Storage; make use crud through curl

--- lib/kvstore/router.ex
+++ lib/kvstore/router.ex
@@ -5,7 +5,11 @@ defmodule KVstore.Router do
   plug(:dispatch)
 
   get("/favicon.ico", do: send_resp(conn, 200, 'Favicon spike'))
+  get("/index", do: KVstore.Storage.index(conn))
   get("/new", do: KVstore.Storage.new(conn))
+  get("/show/:key", do: KVstore.Storage.show(conn))
+  put("/update/:key", do: KVstore.Storage.update(conn))
+  delete("/destroy/:key", do: KVstore.Storage.destroy(conn))
   post("/create", do: KVstore.Storage.create(conn))
 
   get("/", do: send_resp(conn, 200, "Welcome to KVstore"))
--- lib/kvstore/storage.ex
+++ lib/kvstore/storage.ex
@@ -6,10 +6,26 @@ defmodule KVstore.Storage do
 
   def call(conn, _opts), do: conn
 
+  def index(conn) do
+    KVstore.Utils.open_table()
+    kvlists = :dets.match(:kvstore, {:"$1", :"$2", :"$3"})
+    KVstore.Utils.close_table()
+    send_resp(conn, 200, "Listing kvstore records\n#{Kernel.inspect(kvlists)}")
+  end
+
+  def show(conn) do
+    KVstore.Utils.open_table()
+    kvlist = :dets.match_object(:kvstore, {conn.params["key"], :"$2", :"$3"})
+    KVstore.Utils.close_table()
+    send_resp(conn, 200, "Show kvstore record\n#{Kernel.inspect(kvlist)}")
+  end
+
   def new(conn) do
-    conn
-    |> put_resp_header("content-type", "text/html; charset=utf-8")
-    |> send_file(200, "lib/web/new.html")
+    # Use when CRUD in a web interface
+    # conn
+    # |> put_resp_header("content-type", "text/html; charset=utf-8")
+    # |> send_file(200, "lib/web/new.html")
+    send_resp(conn, 200, "You can make request to create action with params")
   end
 
   def create(conn) do
@@ -25,5 +41,27 @@ defmodule KVstore.Storage do
     KVstore.Utils.close_table()
     KVstore.Utils.run_async_task(conn.params["key"], conn.params["ttl"])
     send_resp(conn, 200, "Saved in DETS")
+  end
+
+  def update(conn) do
+    conn =
+      conn
+      |> put_req_header("content-type", "application/x-www-form-urlencoded")
+      |> KVstore.Utils.parse()
+    {:ok, table} = KVstore.Utils.open_table()
+    :dets.insert_new(table, {conn.params["new_key"],
+                             conn.params["value"],
+                             conn.params["ttl"]})
+    :dets.delete(table, conn.params["key"])
+    KVstore.Utils.close_table()
+    KVstore.Utils.run_async_task(conn.params["new_key"], conn.params["ttl"])
+    send_resp(conn, 200, "Updated in DETS")
+  end
+
+  def destroy(conn) do
+    {:ok, table} = KVstore.Utils.open_table()
+    :dets.delete(table, conn.params["key"])
+    KVstore.Utils.close_table()
+    send_resp(conn, 200, "Removed record with #{conn.params["key"]}")
   end
 end
