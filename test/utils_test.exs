fd285b7136e0cc449adb4f82e510180bdedce8c7
Alexander Marychev <killondark@gmail.com>
2018-07-23 14:05:55 +0300

    Updated utils_test

--- test/utils_test.exs
+++ test/utils_test.exs
@@ -11,7 +11,7 @@ defmodule KVstore.UtilsTest do
     :dets.insert_new(table, {"test_key", "test_value", "1"})
     assert :dets.match(:kvstore_test, {:"$1", :"_", :"_"}) == [["test_key"]]
     Utils.remove("test_key", "1")
-    :timer.sleep(:timer.seconds(2))
+    :timer.sleep(:timer.seconds(3))
     assert :dets.match(:kvstore_test, {:"$1", :"_", :"_"}) == []
   end
 
@@ -19,7 +19,7 @@ defmodule KVstore.UtilsTest do
     {:ok, table} = Utils.open_table()
     :dets.insert_new(table, {"test_key", "test_value", "1"})
     Utils.check_ttl_in_dets()
-    :timer.sleep(:timer.seconds(2))
+    :timer.sleep(:timer.seconds(3))
     assert :dets.match(:kvstore_test, {:"$1", :"_", :"_"}) == []
   end
 end
